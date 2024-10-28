0f5dae6cdc9423c76327a16765424c4a16790b65
Erik C. Thauvin <erik@thauvin.net>
2024-10-28 10:15:48 -0700

    Updated dependencies
    Updated dependencies
    
    Bumped JUnit version to 5.11.3
    Bumped PMD extension version to 1.1.7
    Bumped JDK to version 23 (GitHub CI Workflow)

--- src/bld/java/rife/bld/extension/TestNgOperationBuild.java
+++ src/bld/java/rife/bld/extension/TestNgOperationBuild.java
@@ -46,8 +46,8 @@ public class TestNgOperationBuild extends Project {
 
         scope(test)
                 .include(dependency("org.testng", "testng", version(7, 10, 2)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 0)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 0)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 3)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 3)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 26, 3)));
 
         javadocOperation()
