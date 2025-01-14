8632c7525cc1d7b8ccaaf4d74a27f31c6061f9d2
Erik C. Thauvin <erik@thauvin.net>
2025-01-14 11:36:31 -0800

    Bumped bld to version 2.2.0

--- src/bld/java/rife/bld/extension/TestNgOperationBuild.java
+++ src/bld/java/rife/bld/extension/TestNgOperationBuild.java
@@ -39,10 +39,11 @@ public class TestNgOperationBuild extends Project {
 
         downloadSources = true;
         autoDownloadPurge = true;
+        
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(2, 1, 0)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 0)));
 
         scope(test)
                 .include(dependency("org.testng", "testng", version(7, 10, 2)))
