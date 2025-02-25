98ea8a7812ac2f449eabc4b2b5e9bf7bbbc52b71
Erik C. Thauvin <erik@thauvin.net>
2025-02-24 23:20:51 -0800

    Bump JUnit to version 5.12.0

--- src/bld/java/rife/bld/extension/TestNgOperationBuild.java
+++ src/bld/java/rife/bld/extension/TestNgOperationBuild.java
@@ -47,8 +47,8 @@ public class TestNgOperationBuild extends Project {
 
         scope(test)
                 .include(dependency("org.testng", "testng", version(7, 11, 0)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 0)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
