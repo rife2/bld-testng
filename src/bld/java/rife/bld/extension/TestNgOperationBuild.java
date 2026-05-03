706abe0a3e458b61cf7d24f971606ec5de036493
Erik C. Thauvin <erik@thauvin.net>
2026-05-03 10:47:54 -0700

    Version 1.2.0

--- src/bld/java/rife/bld/extension/TestNgOperationBuild.java
+++ src/bld/java/rife/bld/extension/TestNgOperationBuild.java
@@ -34,7 +34,7 @@ public class TestNgOperationBuild extends Project {
     public TestNgOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-testng";
-        version = version(1, 2, 0, "SNAPSHOT");
+        version = version(1, 2, 0);
 
         javaRelease = 17;
 
